fe0f62a3c0e472d3d20651ee152c6028902de70c
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-08 10:11:15 +0700

    Create switchcase.dart

---
+++ switchcase.dart
@@ -0,0 +1,19 @@
+//INI ADALAH CONTOH SWITCHCASE
+ void main(){
+   var nilai = 'E';
+
+   switch(nilai){
+     case 'A':
+     print('baik sekali');
+     break;
+     case 'B':
+     case 'C':
+     print('baik');
+     break;
+     case 'D':
+     print('mantap bro');
+     break;
+     default:
+     print('kasian mana masih muda');
+   }
+ }
